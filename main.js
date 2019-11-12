0dac16cf5276e1b508d0b2f70f444ef92ce92ee7
robschroeder333 <theguyonthecouch7@gmail.com>
2019-11-12 13:36:55 -0800

    feat: create level

--- main.js
+++ main.js
@@ -1,52 +1,59 @@
-const source = document.getElementById('source')
+const assignAttributes = (element, attributes) => {
+	Object.keys(attributes).forEach(key => element.setAttribute(key, attributes[key]))
+}
 
-const target1 = document.getElementById('target1')
-const offset1 = target1.getAttribute('r')
-const target2 = document.getElementById('target2')
-const offset2 = target2.getAttribute('height')
-let pos1 = [100, 100]
-let pos2 = [100, 100]
-let reverse1 = false
-let reverse2 = false
-const limit = source.getAttribute('height')
+const source = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+assignAttributes(source, {
+	'id': 'source',
+	'height': window.innerHeight,
+	'width': window.innerWidth,
+	'style': 'background: black'
+})
+document.body.appendChild(source)
 
-const test = (event) => {
+const ground = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+assignAttributes(ground, {
+	'width': window.innerWidth,
+	'height': window.innerHeight / 10,
+	'x': 0,
+	'y': window.innerHeight - (window.innerHeight / 10),
+	'stroke': 'green',
+	'fill': 'brown'
+})
+source.appendChild(ground)
+
+const baseArray = []
+
+for (let index = 1; index <= 4; index++) {
+	const base = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+	assignAttributes(base, {
+		'id': `base${index}`,
+		'width': window.innerWidth / 10,
+		'height': window.innerHeight / 10,
+		'stroke': 'blue',
+		'fill': 'green',
+		'y': window.innerHeight - ((window.innerHeight / 10) + (window.innerHeight / 20)),
+		'x': ((window.innerWidth / 5) * index ) - ((window.innerWidth / 10) / 2)
+	})
+	baseArray.push(base)
+}
+baseArray.forEach(el => source.appendChild(el))
+
+const target = (event) => {
 	const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-	rect.setAttribute('width', 10)
-	rect.setAttribute('height', 10)
-	rect.setAttribute('x', event.clientX - 10)
-	rect.setAttribute('y', event.clientY - 10)
-	rect.setAttribute('stroke', 'green')
-	rect.setAttribute('fill', 'none')
+	assignAttributes(rect, {
+		'width': 10,
+		'height': 10,
+		'x': event.clientX - 13,
+		'y': event.clientY - 13,
+		'stroke': 'green',
+		'fill': 'none'
+	})
 
 	source.appendChild(rect)
 }
-document.addEventListener('click', test)
+
+document.addEventListener('click', target)
 setInterval(() => {
 
-	if (reverse1) {
-		pos1[0] -= 2
-		if (pos1[0] < 0 + offset1) {
-			reverse1 = !reverse1
-		}		
-	}else{
-		pos1[0] += 2
-		if (pos1[0] > limit - offset1) {
-			reverse1 = !reverse1
-		}		
-	}
-	target1.setAttribute('cx', pos1[0])
-
-	if (reverse2) {
-		pos2[1] -= 2
-		if (pos2[1] < 0) {
-			reverse2 = !reverse2
-		}		
-	}else{
-		pos2[1] += 2
-		if (pos2[1]> limit - offset2) {
-			reverse2 = !reverse2
-		}		
-	}
-	target2.setAttribute('y', pos2[1])	
 }, 20)
