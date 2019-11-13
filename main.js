c31398f0083a6f9105be18e0b59b7ce54089a02b
robschroeder333 <theguyonthecouch7@gmail.com>
2019-11-13 14:46:31 -0800

    refactor: target and level using canvas

--- main.js
+++ main.js
@@ -1,104 +1,137 @@
 
-const source = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+const source = document.createElement('canvas')
 assignAttributes(source, {
 	id: 'source',
 	height: window.innerHeight,
-	width: window.innerWidth,
-	style: 'background: black'
+	width: window.innerWidth
 })
 document.body.appendChild(source)
 
-const ground = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-assignAttributes(ground, {
-	width: window.innerWidth,
-	height: window.innerHeight / 10,
-	x: 0,
-	y: window.innerHeight - (window.innerHeight / 10),
-	stroke: 'green',
-	fill: 'brown'
-})
-source.appendChild(ground)
+const context = source.getContext('2d', {alpha: 'false'})
+
 
 const baseArray = []
-
 for (let index = 1; index <= 4; index++) {
-	const base = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-	assignAttributes(base, {
-		id: `base${index}`,
-		width: window.innerWidth / 10,
-		height: window.innerHeight / 10,
-		stroke: 'blue',
-		fill: 'green',
-		y: window.innerHeight - ((window.innerHeight / 10) + (window.innerHeight / 20)),
-		x: ((window.innerWidth / 5) * index ) - ((window.innerWidth / 10) / 2)
-	})
+	const base = {
+		origin: [
+			((window.innerWidth / 5) * index ) - ((window.innerWidth / 10) / 2),
+			window.innerHeight - ((window.innerHeight / 10) + (window.innerHeight / 20))
+		],
+		health: 2,
+		dimensions: [
+			window.innerWidth / 10,
+			window.innerHeight / 10
+		],
+		color: () => {
+			switch (base.health) {
+			case 2:
+				return 'green'				
+			case 1:
+				return 'yellow'				
+			case 0:
+				return 'red'						
+			default:
+				return 'red'						
+			}
+		}
+	}
 	baseArray.push(base)
 }
-baseArray.forEach(el => source.appendChild(el))
 
+
+const targetArray = []
 const target = (event) => {
-	const speed = 5
-	const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-	assignAttributes(rect, {
-		width: 10,
-		height: 10,
-		x: event.clientX - 13,
-		y: event.clientY - 13,
-		stroke: 'green',
-		fill: 'none'
-	})
-	source.appendChild(rect)
+	const target = {
+		cycles: 1000,
+		origin: [
+			event.clientX,
+			event.clientY
+		],
+		reticle: {
+			dimensions: [
+				10,
+				10
+			],
+			color: 'green'
+		},
+		line: {
+			start: weighBases(event.clientX),
+			color: 'yellow',
+			length: 2			
+		},
+		explosionSize: 20,
+		expolosionColor: 'yellow',
+	}
+	target.reticle.offset = [
+		target.origin[0] - target.reticle.dimensions[0] - 3,
+		target.origin[1] - target.reticle.dimensions[1] - 3
+	]
+	target.line.vector = [
+		target.origin[0] - target.line.start[0],
+		target.origin[1] - target.line.start[1]		
+	]
+	target.line.magnitude = Math.sqrt(Math.pow(target.line.vector[0], 2) + Math.pow(target.line.vector[1], 2))
+	target.line.vector = [
+		target.line.vector[0] / target.line.magnitude, 
+		target.line.vector[1] / target.line.magnitude
+	]
+	target.line.end = [
+		target.line.start[0] + (target.line.vector[0] * target.line.length), 
+		target.line.start[1] * (target.line.vector[1] * target.line.length)
+	]
 	
-	const origin = weighBases(event.clientX)
-	const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-	assignAttributes(line, {
-		id: Math.random() * 200,
-		x1: origin[0],
-		y1: origin[1],
-		stroke: 'yellow',
-	})
-	const drawLineX = document.createElementNS('http://www.w3.org/2000/svg', 'animate')
-	assignAttributes(drawLineX, {
-		attributeName: 'x2',
-		values: `${origin[0]};${event.clientX}`,
-		dur: `${speed}s`,
-		fill: 'freeze'
-	})
-	const drawLineY = document.createElementNS('http://www.w3.org/2000/svg', 'animate')
-	assignAttributes(drawLineY, {
-		attributeName: 'y2',
-		values: `${origin[1]};${event.clientY}`,
-		dur: `${speed}s`,
-		fill: 'freeze'
-	})
-	line.appendChild(drawLineX)
-	line.appendChild(drawLineY)
-	
-	const expolosion = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-	assignAttributes(expolosion, {
-		cx: event.clientX,
-		cy: event.clientY,
-		stroke: 'green',
-		fill: 'yellow'
-	})
-	const explode = document.createElementNS('http://www.w3.org/2000/svg', 'animate')
-	assignAttributes(explode, {
-		attributeName: 'r',
-		values: '0;20;0',
-		duration: '3s'
-	})
-	expolosion.appendChild(explode)
-
-	drawLineX.addEventListener('endEvent',() => rect.append(expolosion))
-	source.appendChild(line)
+	target.draw = () => {
+		if (target.cycles > 0) {				
+			context.strokeStyle = target.reticle.color
+			context.strokeRect(
+				target.reticle.offset[0], 
+				target.reticle.offset[1], 
+				target.reticle.dimensions[0],
+				target.reticle.dimensions[1]
+			)	
+			
+			if (target.cycles < 900 && target.cycles > 500) {
+				context.strokeStyle = target.line.color
+				context.beginPath()
+				context.moveTo(target.line.start[0], target.line.start[1])
+				context.lineTo(target.line.end)
+				context.stroke()
+				target.line.end += target.vector
+			}
+			target.cycles--
+		} else {
+			target
+		}
+	}
+	targetArray.push(target)
 }
 
 document.addEventListener('click', target)
 
-
 setInterval(() => {
-	
-}, 20)
+	context.fillStyle = 'black'
+	context.fillRect(0, 0, window.innerWidth, window.innerHeight) // background
+	context.fillStyle = 'brown'
+	context.fillRect( //ground
+		0, 
+		window.innerHeight - (window.innerHeight / 10), 
+		window.innerWidth, 
+		window.innerHeight / 10
+	)
+	baseArray.forEach((ele) => {
+		context.fillStyle = ele.color()
+		context.fillRect(ele.origin[0], ele.origin[1], ele.dimensions[0], ele.dimensions[1])
+	})
+
+	targetArray.forEach((ele, i) => {
+		if (ele.cycles > 0) {
+			ele.draw()
+		} else {
+			//remove
+		}
+		
+	})
+}, 100)
 
 function weighBases(targetX) {
 	let result = []
@@ -106,19 +139,19 @@ function weighBases(targetX) {
 	let lowest
 
 	baseArray.forEach((base, i) => {
-		const weight = Math.abs(targetX - base.getAttribute('x'))
+		const weight = Math.abs(targetX - base.origin[0])
 		if (choice === undefined){
 			choice = i
 			lowest = weight
 		} else {
-			Math.abs(targetX - base.getAttribute('x'))
+			Math.abs(targetX - base.origin[0])
 			if (lowest > weight) {
 				lowest = weight
 				choice = i
 			}
 		}
 	})
-	result = [baseArray[choice].getAttribute('x'), baseArray[choice].getAttribute('y')]
+	result = [baseArray[choice].origin[0], baseArray[choice].origin[1]]
 	return result
 }
 function assignAttributes(element, attributes) {
